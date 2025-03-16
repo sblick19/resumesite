da383a7902ac1af07d7f5e69dfe0e2658bd5086b
sblick19 <158088003+sblick19@users.noreply.github.com>
2025-03-15 21:04:15 -0500

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -3,7 +3,7 @@ import { GitContentSource } from "@stackbit/cms-git";
 
 export default defineStackbitConfig({
   stackbitVersion: "~0.6.0",
-  ssgName: "nextjs",
+  ssgName: "html",
   nodeVersion: "18",
   contentSources: [
     new GitContentSource({
